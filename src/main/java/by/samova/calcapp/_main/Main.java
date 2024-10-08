03c94f886401df4fa42330677cdf252e2544f8cf
ANKA <annamaslovaspb@gmail.com>
2024-10-09 00:21:05 +0300

    great calculator)

--- src/main/java/by/samova/calcapp/_main/Main.java
+++ src/main/java/by/samova/calcapp/_main/Main.java
@@ -2,39 +2,40 @@ package by.samova.calcapp._main;
 
 import by.samova.calcapp.service.ResultWriterService;
 import by.samova.calcapp.util.Calculator;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    sc.useLocale(Locale.US);
-    System.out.println("Введите первое число:");
-    double x = sc.nextDouble();
-    System.out.println("Введите второе число:");
-    double y = sc.nextDouble();
-    System.out.println("Выберите операцию (+, -, *, /)");
-    char operation = sc.next().charAt(0);
-    String operDescript = "";
-    double res = 0;
-    switch (operation) {
-        case '+':
-        res = Calculator.sumUp(x,y);
-        operDescript = "Операция сложения: ";
-        break;
-        case '-':
-        res = Calculator.subtract(x,y);
-        operDescript = "Операция вычитания: ";
-        break;
-        case '*':
-        res = Calculator.multiply(x,y);
-        operDescript = "Операция умножения: ";
-        break;
-        case '/':
-        res = Calculator.division(x,y);
-        operDescript = "Операция деления: ";
-        break;
-    }
-    ResultWriterService.printResult(operDescript, x, y, operation, res);
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        System.out.println("Введите первое число:");
+        double x = sc.nextDouble();
+        System.out.println("Введите второе число:");
+        double y = sc.nextDouble();
+        System.out.println("Выберите операцию (+, -, *, /)");
+        char operation = sc.next().charAt(0);
+        String operDescript = "";
+        double res = 0;
+        switch (operation) {
+            case '+':
+                res = Calculator.sumUp(x, y);
+                operDescript = "Операция сложения: ";
+                break;
+            case '-':
+                res = Calculator.subtract(x, y);
+                operDescript = "Операция вычитания: ";
+                break;
+            case '*':
+                res = Calculator.multiply(x, y);
+                operDescript = "Операция умножения: ";
+                break;
+            case '/':
+                res = Calculator.division(x, y);
+                operDescript = "Операция деления: ";
+                break;
+        }
+        ResultWriterService.printResult(operDescript, x, y, operation, res);
     }
 }
--- src/main/java/by/samova/calcapp/util/Calculator.java
+++ src/main/java/by/samova/calcapp/util/Calculator.java
@@ -4,12 +4,15 @@ public class Calculator {
     public static double sumUp(double x, double y) {
         return x + y;
     }
+
     public static double subtract(double x, double y) {
         return x - y;
     }
+
     public static double multiply(double x, double y) {
         return x * y;
     }
+
     public static double division(double x, double y) {
         return x / y;
     }
